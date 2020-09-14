f020a1c2f5092e65a6960cc48548acbb198301ae
jacoe0850 <jacoe0850@wrdsb.ca>
2020-09-14 05:19:48 +0000

    Finished

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 /**
- *
- * @author 
+ * This program prints hello world to the screen
+ * @jaoce0850 
  */
 public class Main {
 
@@ -9,7 +9,7 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+   // Print "Hello World!"" to the screen
+    System.out.println("Hello World!");
   }
 }
